09de56fffe3c913148f2ffa982abc411232e0804
len-kami <lkami@bogital.com>
2023-08-04 23:27:01 +0100

    Added Header and helper components to make it work

---
+++ frontend/components/Header.tsx
@@ -0,0 +1,51 @@
+import Logo from "./Logo";
+
+type Props = {
+
+}
+
+const Header: React.FC<Props> = ({ }) => {
+    return (
+        <div className="w-full
+        bg-[url('http://localhost:3000/home-image.png')]
+        hero-base sm:hero-sm xl:hero-xl
+        ">
+            <header
+                className="container sm:w-10/12 xl:w-9/12 mx-auto"
+            >
+                <nav className="flex justify-between pr-4">
+                    <Logo />
+                    <ul className="hidden sm:flex space-x-4 flex-1 items-center justify-start pl-4">
+                        <li className="font-semibold">Accueil</li>
+                        <li className="">Suivis des commandes</li>
+                        <li className="">Boutique</li>
+                    </ul>
+                    <img className="sm:hidden" src="/hamburger.svg" />
+                </nav>
+
+                <div className="text-black space-y-8 pt-16 px-8 text-center sm:text-left sm:px-0 sm:pt-16 md:w-8/12 lg:w-7/12 xl:w-1/2">
+
+                    <h1 className="text-4xl sm:text-6xl">
+                        Simplifiez votre rentrée scolaire avec <span className="text-[#470047]">Cahierix</span>
+                    </h1>
+                    <p className="text-[18px] sm:text-xl">
+                        Envoyez-nous votre liste d'effets scolaires, on s'occupe de tout ! Nous pratiquons les meilleurs coûts sur le marché camerounais. Nous vous repondons en moins de 15mins.
+                    </p>
+
+                </div>
+
+                <div className=" flex flex-col space-y-4 items-center sm:flex-row sm:items-start sm:space-y-0 space-x-4 py-8 pb-16 text-white">
+                    <button className="px-4 py-4 lg:px-8 text-base lg:text-xl rounded-lg bg-[#470047]">
+                        Envoyez nous votre liste
+                    </button>
+                    <button className="px-4 py-4 lg:px-8 text-base lg:text-xl rounded-lg border-2 border-white">
+                        Liste des manuels
+                    </button>
+                </div>
+
+            </header>
+        </div>
+    )
+}
+
+export default Header;
---
+++ frontend/components/Logo.tsx
@@ -0,0 +1,17 @@
+// import classnames from 'classnames';
+
+type Props = {
+
+}
+
+const Logo: React.FC<Props> = ({ }) => {
+    return (
+        <div className="flex justify-center items-center w-24 h-24 sm:w-40 sm:h-32 bg-[#470047]">
+            <p className="text-white uppercase text-base sm:text-2xl sm:font-bold">
+                Cahierix
+            </p>
+        </div>
+    )
+}
+
+export default Logo;
