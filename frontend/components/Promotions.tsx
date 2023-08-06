42c9ad423fbbee68465312178b1d8d0115496f36
len-kami <lkami@bogital.com>
2023-08-06 01:22:09 +0100

    - Updated promotions section

--- frontend/components/Promotions.tsx
+++ frontend/components/Promotions.tsx
@@ -6,20 +6,26 @@ type Props = {
 
 const Promotions: React.FC<Props> = ({ }) => {
     return (
-        <section className="w-full bg-[#ffbb00]">
-            <div className="section mx-auto text-white">
-                <h2 className="text-4xl text-center mb-8 text-black">Promotions</h2>
+        <section className="w-full bg-[url('http://localhost:3000/promotion.png')] bg-cover">
+            <div className="section mx-auto  text-white">
 
-                <div className="text-center">
-                    <p className="">
-                        Livraisons à <br />
-                        <span className="text-blue-800 font-bold">
-                            <span className="text-[8rem]">0</span>
-                            <span className="text-3xl inline-block -translate-y-24">FCFA</span>
-                        </span>
-                        <br />
-                        Pour les commandes supérieure à <span className="text-blue-800 font-semibold">99 900 FCFA</span>.
-                    </p>
+                <div className="mx-4 md:mx-0">
+
+                    <h2 className="text-4xl text-center mb-8 font-semibold text-[#004080]">Promotions</h2>
+
+                    <div className="text-center">
+                        <p>
+                            <span className="text-2xl inline-block pb-4">Livraisons à</span> <br />
+                            <span className="text-[#004080] font-bold">
+                                <span className="text-[8rem]">0</span>
+                                <span className="text-5xl inline-block -translate-y-24">FCFA</span>
+                            </span>
+                            <br />
+                            <span className="text-2xl">
+                                Pour les commandes supérieure à <br className="md:hidden" /> <span className="text-[#004080] font-bold line">99 900 FCFA</span>
+                            </span>
+                        </p>
+                    </div>
 
                 </div>
 
