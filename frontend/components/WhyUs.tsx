49d6fa1ce9e438b6e36962dc2b71501c07afc644
len-kami <lkami@bogital.com>
2023-08-06 02:12:44 +0100

    Updated WhyUs section

--- frontend/components/WhyUs.tsx
+++ frontend/components/WhyUs.tsx
@@ -6,45 +6,56 @@ type Props = {
 
 const WhyUs: React.FC<Props> = ({ }) => {
     return (
-        <section className="w-full bg-[url('http://localhost:3000/home-banner.jpg')] whyus-hero md:whyus-hero-md lg:whyus-hero-lg">
-            <div className="section mx-auto">
-                <h2 className="text-4xl text-center mb-8">Pourquoi choisir Cahierix</h2>
+        <section className="w-full relative">
+            
+            <div className="absolute left-0 top-0 right-1/2 -z-10 bottom-0 bg-[#FAABAA] md:bg-[url('http://localhost:3000/why-cahierix.png')] md:bg-cover">
 
-                <div className="space-y-8 md:m-whyus lg:m-whyus-lg">
-                    <div className="flex items-center space-x-4">
-                        <span className="number-label text-center">
-                            1
-                        </span>
-                        <p className="mt-4 text-lg md:text-base">
-                            Envoyez-nous votre liste d'effets scolaires via notre site internet ou whatsapp au 693 369 xxx.
-                        </p>
-                    </div>
-                    <div className="flex items-center space-x-4">
-                        <span className="number-label text-center">
-                            2
-                        </span>
-                        <p className="mt-4 text-lg md:text-base">
-                            Notre équipe d'expert prépare soigneusement votre commande.
-                        </p>
-                    </div>
-                    <div className="flex items-center space-x-4">
-                        <span className="number-label text-center">
-                            3
-                        </span>
-                        <p className="mt-4 text-lg md:text-base">
-                            Recevez vos cahiers et livres directements chez vous, prêt pour la rentrée.
-                        </p>
-                    </div>
-                    <div className="flex items-center space-x-4">
-                        <span className="number-label text-center">
-                            4
-                        </span>
-                        <p className="mt-4 text-lg md:text-base">
-                            Notre équipe d'expert prépare soigneusement votre commande.
-                        </p>
+            </div>
+
+            <div className="section mx-auto z-20">
+
+                <div className="mx-4 md:mx-0">
+                    <h2 className="text-4xl text-center mb-8 font-semibold">Pourquoi choisir Cahierix</h2>
+
+                    <div className="space-y-8 md:m-whyus">
+                        <div className="flex items-center space-x-4">
+                            <span className="number-label text-center">
+                                1
+                            </span>
+                            <p className="mt-4 text-lg md:text-base">
+                                Envoyez-nous votre liste d'effets scolaires via notre site internet ou whatsapp au 693 369 xxx.
+                            </p>
+                        </div>
+                        <div className="flex items-center space-x-4">
+                            <span className="number-label text-center">
+                                2
+                            </span>
+                            <p className="mt-4 text-lg md:text-base">
+                                Notre équipe d'expert prépare soigneusement votre commande.
+                            </p>
+                        </div>
+                        <div className="flex items-center space-x-4">
+                            <span className="number-label text-center">
+                                3
+                            </span>
+                            <p className="mt-4 text-lg md:text-base">
+                                Recevez vos cahiers et livres directements chez vous, prêt pour la rentrée.
+                            </p>
+                        </div>
+                        <div className="flex items-center space-x-4">
+                            <span className="number-label text-center">
+                                4
+                            </span>
+                            <p className="mt-4 text-lg md:text-base">
+                                Notre équipe d'expert prépare soigneusement votre commande.
+                            </p>
+                        </div>
                     </div>
                 </div>
+
             </div>
+
+            
         </section>
     )
 }
