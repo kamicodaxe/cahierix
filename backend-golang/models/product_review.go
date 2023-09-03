d9b8db39136a4e46775fbd363588907312b1d2db
Loïc Kami <lkami@bogial.com>
2023-09-03 19:37:39 +0200

    Updated product_review.go model

--- backend-golang/models/product_review.go
+++ backend-golang/models/product_review.go
@@ -6,7 +6,12 @@ import (
 
 type ProductReview struct {
 	gorm.Model
-	UserID    uint   `json:"userId"`
+	ID        uint   `gorm:"primaryKey" json:"id"`
+	UserID    string `json:"userId"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+	Contact   string `json:"contact"`
 	ProductID uint   `json:"productId"`
 	Rating    int    `json:"rating"`
 	Review    string `gorm:"type:varchar(128);not null" json:"review"`
